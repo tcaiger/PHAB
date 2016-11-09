c84136dd38077caa95904440593047c901b8f0a8
Tom Caiger <caigertom@gmail.com>
2016-11-09 21:08:55 +1300

    Add Peers Page

---
+++ mysite/admin/PeersPageAdmin.php
@@ -0,0 +1,9 @@
+<?php
+
+class PeersPageAdmin extends SinglePageAdmin {
+
+    private static $menu_title = 'PeersPage';
+    private static $tree_class = 'PeersPage';
+    private static $url_segment = 'peers-page';
+    private static $menu_icon = 'mysite/images/file-icon.png';
+}
